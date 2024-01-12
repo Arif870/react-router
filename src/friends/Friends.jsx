3b6fdb1cff788bd05d0abc31c0d79cb1f454421f
Arif870 <arifuzzaman959@gmail.com>
2024-01-12 22:50:03 +0600

    useNaviagtion for loaded

--- src/friends/Friends.jsx
+++ src/friends/Friends.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useNavigation } from "react-router-dom";
 
 const Friends = () => {
   const employees = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <div>
+      {navigation.state === "loading" ? "loading" : ""}
       <h3>Employees List</h3>
       {employees.length === 0 && "loading.."}
       {employees.map(employee => (
