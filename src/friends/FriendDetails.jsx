17a4c4fc6fc31cfd3b15674284870a1d9237fb64
Arif870 <arifuzzaman959@gmail.com>
2024-01-12 22:23:20 +0600

    useNavigate initiated to go back previous page

--- src/friends/FriendDetails.jsx
+++ src/friends/FriendDetails.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const FriendDetails = () => {
   const friendDetails = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <div>
       <h3>Username : {friendDetails.username}</h3>
       <p>Email: {friendDetails.email}</p>
       <p>Phone: {friendDetails.phone}</p>
+      <button onClick={() => navigate(-1)}>Back</button>
     </div>
   );
 };
